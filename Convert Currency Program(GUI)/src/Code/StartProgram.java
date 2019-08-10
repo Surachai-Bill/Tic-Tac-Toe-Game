39776038589f4a2482bf3c49590949f73c723317
LilThunder <49782983+LilThunder@users.noreply.github.com>
2019-08-10 22:39:41 +0700

    Add files via upload

---
+++ Convert Currency Program(GUI)/src/Code/StartProgram.java
@@ -0,0 +1,19 @@
+
+package Code;
+
+
+public class StartProgram {
+    
+      public static void main(String[] args){
+          
+          StartProgramCodeGUI start = new StartProgramCodeGUI();
+          start.setAlwaysOnTop(true);
+          start.setVisible(true);
+          
+      }
+      
+      public int Test(){
+          return 0;
+          
+      } 
+}
